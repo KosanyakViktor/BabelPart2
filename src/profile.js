7f22f56dbadadcfac9a64d6253d505a3e8c20ca5
Viktor Kosanyak <kosanyakviktor@gmail.com>
2021-01-15 21:24:56 +0200

    c

---
+++ src/profile.js
@@ -0,0 +1,8 @@
+const printProfile = profileData => {
+    const { name, company } = profileData;
+    console.log(`${name} from ${company}`);   
+<<<<<<< HEAD
+};
+=======
+};
+>>>>>>> 5fb2ae3... new commit
